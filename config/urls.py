61d47a09ba9a39b4bf8c845ea3b497278bc87e1f
Atabek Isakov <a.isakov@newuu.uz>
2026-05-03 15:13:58 +0500

    aprel test

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # JWT auth
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Product CRUD
+
     path('api/', include('products.urls')),
 ]
